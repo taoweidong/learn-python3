a8da40604b8ce74ae202b4ba5542289f76535a28
Taoweidong <taowd@outlook.com>
2019-12-08 16:31:00 +0800

    增加变量的说明文件

--- teach/Hello.py
+++ teach/Hello.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     # name = raw_input('输入你的名字:')
     # print 'Hello', name
 
+    # 变量：就是一个存储数据的时候当前数据所在的内存地址的名称或者代号，用于快速访问到这个内存地址
+    # 变量名 = 值
+
     # python是一种动态语言，可以把任意数据类型赋值给变量，同一个变量可以反复赋值，而且可以是不同类型的变量
     # java是一种静态语言，变量定义的时候必须指定数据类型，如果赋值时的数据类型和定义的不匹配会报错
     # 字符串的定义
@@ -24,6 +27,40 @@ if __name__ == '__main__':
     print abc
 
     # 内存的理解
+
+    a = 'ABC'
+    # Python解释器干了两件事：
+    # 1、在内存中创建了一个‘abc’的字符串
+    # 2、在内存中创建了一个名字是'a'的变量，并把它指向'abc'
+    a = 'ABC'
+    b = a
+    a = 'XYZ'
+    print(b)
+
+    # 数学计算演示
+    print 10 / 3
+    print 10.0 / 3
+    print 10 % 3
+
+    # list列表演示:有序的集合，可以随时添加和删除其中的元素
+    listData = ['Hello', 'World']
+    print(listData)
+    # 列表的长度
+    print(len(listData))
+    print(listData[0])
+    print(listData[-1])
+    # 注意月越界风险
+    # print(listData[3])
+
+    # 列表数据的添加
+    listData.append('张三')
+    print(listData)
+
+    # 变量类型查询
+    print(type(listData))
+    print(type(a))
+    print(type(b))
+    print(type(abc))
 
     # 循环语句
     if flag:
