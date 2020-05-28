dc7ca46bbf590903873b74eb40e92db76cde7471
Taoweidong <taowd@outlook.com>
2020-05-28 23:34:06 +0800

    jinja2框架学习-完成基本功能测试

--- web/HelloPython.py
+++ web/HelloPython.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from jinja2 import Environment, FileSystemLoader
+import datetime
 
 
-def generate_html(body, starttime, stoptime):
+def generate_html(bodys, startTime, stopTime):
     env = Environment(loader=FileSystemLoader('./template'))
-    template = env.get_template('template.html')
-    with open("result.html", 'w+') as fout:
-        html_content = template.render(start_time=starttime,
-                                       stop_time=stoptime,
-                                       body=body)
-        fout.write(html_content)
+    template = env.get_template('mytemplate01.html')
+    with open("result.html", 'w+', encoding='utf-8') as outHtml:
+        html_content = template.render(start_time=startTime,
+                                       stop_time=stopTime,
+                                       body=bodys,
+                                       Title='这是一个jinjia2模板引擎的示例代码')
+        outHtml.write(html_content)
 
 
 if __name__ == "__main__":
     body = []
     result = {'cabID': 1, 'shijian': 2019, 'final_result': u"正常", 'info': u"无",
               'image_path': u"test.jpg"}
+    result2 = {'cabID': 2, 'shijian': 2020, 'final_result': u"异常", 'info': u"无",
+               'image_path': u"test.jpg"}
     body.append(result)
-    generate_html(body, 2019, 2019)
+    body.append(result2)
+
+    startTime = datetime.datetime.now().strftime("%Y-%m-%d")
+    stopTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    generate_html(body, startTime, stopTime)
--- web/README.md
+++ web/README.md
@@ -1,3 +1,9 @@
 - http://docs.jinkan.org/docs/jinja2/
 - https://blog.csdn.net/C_teacher/article/details/104659507
 - https://blog.csdn.net/zong596568821xp/article/details/100522584
+
+
+
+# 注意事项
+- 模板文件的文件格式必须是uft-8 否则生成html页面会报错
+- 写入html页面是必须制定写入文件的 encoding='utf-8' 格式，否则，生成的html中文内容为乱码格式
