6c5ffe04b03c458715908bf95a87017f16ae687d
Taoweidong <taowd@outlook.com>
2020-09-06 12:06:01 +0800

    selenium爬虫入门

--- seleniumTest/HelloSelenium.py
+++ seleniumTest/HelloSelenium.py
@@ -18,9 +18,11 @@ if __name__ == '__main__':
 
     driver.switch_to_window(all_hand[-1])
     sleep(3)
-    driver.find_element_by_xpath('//*[@id="nav"]//a[@class="imooc"]').click()
+    driver.find_element_by_xpath('//*[@id="nav"]//a[@href="/read"]').click()
+    result = driver.find_element_by_xpath('//*[@id="main"]/div[2]/div[1]/ul/li/div/a[1]/p[@class="title"]')
+    print(result.text)
     # 鼠标悬停事件
-    ActionChains(driver).move_to_element(driver.find_element_by_xpath(
-        '//*[@id="main"]//div[@class="item" and @data-id="e"]')).perform()
+    # ActionChains(driver).move_to_element(driver.find_element_by_xpath(
+    #     '//*[@id="main"]//div[@class="item" and @data-id="e"]')).perform()
     # driver.refresh()
     # driver.quit()
